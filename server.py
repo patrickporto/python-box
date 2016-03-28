c9a9379e4d1246443fd538e3b544e64958b6f986
Patrick Porto <patrick.s.porto@gmail.com>
2016-03-27 21:39:05 -0300

    Excluindo arquivos do servidor

--- server.py
+++ server.py
@@ -1,12 +1,26 @@
+import json
+import os
 from gevent import pywsgi
 from geventwebsocket.handler import WebSocketHandler
 
 
+DIRECTORY_STORAGE = '/tmp/'
+
+
+def on_deleted(path, is_directory):
+    path = os.path.join(DIRECTORY_STORAGE, path)
+    if is_directory:
+        os.rmdir(path)
+    else:
+        os.remove(path)
+
+
 def websocket_app(environ, start_response):
     ws = environ["wsgi.websocket"]
-    message = ws.receive()
-    print(message)
-    ws.send(message)
+    data = json.loads(ws.receive())
+    if data['type'] == 'DELETE':
+        on_deleted(data['path'], data['is_directory'])
+    ws.send(data)
 
 server = pywsgi.WSGIServer(
     ('0.0.0.0', 9000),
---
+++ websocket.py
@@ -0,0 +1,27 @@
+import json
+from ws4py.client.threadedclient import WebSocketClient
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+class Client(WebSocketClient, FileSystemEventHandler):
+    def opened(self):
+        self.observer = Observer()
+        self.observer.schedule(self, 'test', recursive=True)
+        self.observer.start()
+
+    def on_deleted(self, event):
+        src = '/'.join(event.src_path.split('/')[1:])
+        self.send(json.dumps({
+            'type': 'DELETE',
+            'path': src,
+            'is_directory': event.is_directory,
+        }))
+
+    def closed(self, code, reason=None):
+        self.observer.stop()
+        self.observer.join()
+        print "Closed down", code, reason
+
+    def received_message(self, message):
+        print(message)
