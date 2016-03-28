34baf558566164f86b0e7ff7d1b161539ca3bf2c
Patrick Porto <patrick.s.porto@gmail.com>
2016-03-28 01:28:23 -0300

    Definido diretório pelo argumento do script

--- client.py
+++ client.py
@@ -1,9 +1,23 @@
+# encoding: utf-8
+import os
+from argparse import ArgumentParser
 from websocket import Client
 
-if __name__ == "__main__":
+
+def start_server(path):
     try:
-        ws = Client(url='ws://localhost:9000/', path='test', protocols=['http-only', 'chat'])
+        ws = Client(url='ws://localhost:9000/', path=path, protocols=['http-only', 'chat'])
         ws.connect()
         ws.run_forever()
     except KeyboardInterrupt:
         ws.close()
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument('directory', metavar='DIRETÓRIO', type=str,
+                        help='diretório para sincronização')
+    args = parser.parse_args()
+    if not os.path.exists(args.directory) or not os.path.isdir(args.directory):
+        parser.error("diretório para sincronização inexistente")
+    start_server(args.directory)
